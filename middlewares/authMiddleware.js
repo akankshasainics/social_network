353bd85272a1a1d3760f88a0faad4e211d16e796
Akanksha Saini <akankshasaini03@gmail.com>
2024-08-10 12:02:54 +0530

    Add validation to authorize user

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,7 @@
 const {ApiError} = require("../utils/apiError");
-const mongoose = require("mongoose");
 const {verifyToken} = require("../utils/jwt");
 const {findOneUser} = require("../dataAccess/userAccess");
+const {findOnePost} = require("../dataAccess/postAccess");
 
 const authenticateUser = async (req, res, next) => {
     let token;
@@ -28,4 +28,23 @@ const authenticateUser = async (req, res, next) => {
     }
 }
 
-module.exports = {authenticateUser}
+const authorizeUser = async(req, res, next) => {
+    try {
+        const {joined_groups} = req.user;
+        const {postId} = req.params;
+        const post = await findOnePost({_id: postId});
+        if(!post)
+        {
+            return next(new ApiError('Post does not found.', 404))
+        }
+        if(!joined_groups.find(g => g.equals(post.group_id)))
+        {
+            return next(new ApiError('User is not authorized to perform operation in this group.', 401))
+        }
+        next();
+    } catch(error) {
+        return next(new ApiError())
+    }
+}
+
+module.exports = {authenticateUser, authorizeUser}
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { authenticateUser} = require("../middlewares/authMiddleware");
+const { authenticateUser, authorizeUser} = require("../middlewares/authMiddleware");
 const postController = require("../controllers/post.controller");
 const {createPostValidator, addCommentValidator} = require("../validators/postValidator")
 const {objectIdValidator} = require("../validators/customValidator");
 const {validate} = require("../middlewares/validationMiddleware");
 
 router.post('/', authenticateUser, createPostValidator, validate, postController.createPost);
-router.post('/:postId/like', authenticateUser, objectIdValidator('postId'), validate, postController.likePost);
-router.post('/:postId/comments', authenticateUser, addCommentValidator, validate, postController.commentOnPost)
+router.post('/:postId/like', authenticateUser, authorizeUser, objectIdValidator('postId'), validate, postController.likePost);
+router.post('/:postId/comments', authenticateUser, authorizeUser, addCommentValidator, validate, postController.commentOnPost)
 
 module.exports = router;
